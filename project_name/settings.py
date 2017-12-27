467d8bc7f2fe4f0279b4c63ce3322fb2196114aa
Bryan Arnold <PureMath86@users.noreply.github.com>
2017-12-27 14:55:53 -0600

    Update settings.py

--- project_name/settings.py
+++ project_name/settings.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "{{ secret_key }}"
+SECRET_KEY = "s3cr3t_key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
